0092a7a1fed06fddb62524beae7d58e1bf02708c
yanai <yanai@tikalk.com>
2015-12-22 09:20:19 +0200

    typo

--- run-bin/management.sh
+++ run-bin/management.sh
@@ -6,4 +6,4 @@ DIRNAME=`dirname $0`
 APP_HOME=`cd $DIRNAME/..;pwd;`
 export APP_HOME;
 
-java -Duser.timezone="UTC" -jar $APP_HOME/build/libs/as-managemet.jar
+java -Duser.timezone="UTC" -jar $APP_HOME/build/libs/as-management.jar
