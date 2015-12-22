d0e6512969c9c6669a4a8291641d676dcc25d277
yanai <yanai@tikalk.com>
2015-12-22 09:05:47 +0200

    move GPS

--- src/main/java/com/tikal/fleettracker/management/Application.java
+++ src/main/java/com/tikal/fleettracker/management/Application.java
@@ -28,16 +28,20 @@ public class Application {
 			public void run(final String... args) throws Exception {
 				final Vehicle a1 = vehicleRepository.save(new Vehicle("111-aaa","Mazda3"));
 				final Vehicle b1 = vehicleRepository.save(new Vehicle("111-bbb","Mazda6"));
-				guardianRepository.save(new Guardian("g1", "g1","yanaif@gmail.com").addVehicle(a1).addVehicle(b1));
-				deviceRepository.save(new Device("013950004346194", a1));
+				final Vehicle c1 = vehicleRepository.save(new Vehicle("111-ccc","Volvo"));
+				guardianRepository.save(new Guardian("g1", "g1","yanaif@gmail.com").addVehicle(a1).addVehicle(b1).addVehicle(c1));
+				deviceRepository.save(new Device("1131", a1));
+				deviceRepository.save(new Device("1277", b1));
+				deviceRepository.save(new Device("2237", c1));
 				
-				final Vehicle a2 = vehicleRepository.save(new Vehicle("222-aaa","Honda Civic"));
-				guardianRepository.save(new Guardian("g2", "g2","yanaif@gmail.com").addVehicle(a2));
-				deviceRepository.save(new Device("863158021855913", a2));
 				
-				final Vehicle a3 = vehicleRepository.save(new Vehicle("333-aaa","Honda Civic"));
-				guardianRepository.save(new Guardian("g3", "g3","yanaif@gmail.com").addVehicle(a3));
-				deviceRepository.save(new Device("013950004334497", a3));
+//				final Vehicle a2 = vehicleRepository.save(new Vehicle("222-aaa","Honda Civic"));
+//				guardianRepository.save(new Guardian("g2", "g2","yanaif@gmail.com").addVehicle(a2));
+//				deviceRepository.save(new Device("863158021855913", a2));
+//				
+//				final Vehicle a3 = vehicleRepository.save(new Vehicle("333-aaa","Honda Civic"));
+//				guardianRepository.save(new Guardian("g3", "g3","yanaif@gmail.com").addVehicle(a3));
+//				deviceRepository.save(new Device("013950004334497", a3));
 				
 			}
 		};
